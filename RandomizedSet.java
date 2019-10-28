35d2b94755e79c068db6bf707124c41952f62fb7
linqig <linqig@amazon.com>
2019-10-27 21:21:48 -0700

    Update RandomizedSet.java

--- RandomizedSet.java
+++ RandomizedSet.java
@@ -1,32 +1,80 @@
+/*
+
+380. Insert Delete GetRandom O(1)
+Medium
+
+Design a data structure that supports all following operations in average O(1) time.
+
+insert(val): Inserts an item val to the set if not already present.
+remove(val): Removes an item val from the set if present.
+getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+Example:
+
+// Init an empty set.
+RandomizedSet randomSet = new RandomizedSet();
+
+// Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomSet.insert(1);
+
+// Returns false as 2 does not exist in the set.
+randomSet.remove(2);
+
+// Inserts 2 to the set, returns true. Set now contains [1,2].
+randomSet.insert(2);
+
+// getRandom should return either 1 or 2 randomly.
+randomSet.getRandom();
+
+// Removes 1 from the set, returns true. Set now contains [2].
+randomSet.remove(1);
+
+// 2 was already in the set, so return false.
+randomSet.insert(2);
+
+// Since 2 is the only number in the set, getRandom always return 2.
+randomSet.getRandom();
+*/
 class RandomizedSet {
-    Set<Integer> set;
-    List<Integer> list;
-    Random random;
+    ArrayList<Integer> nums;
+    HashMap<Integer, Integer> num2index;
+    Random rand;
+    
+    /** Initialize your data structure here. */
     public RandomizedSet() {
-        set = new HashSet<>();
-        list = new ArrayList<>();
-        random = new Random();
+        nums = new ArrayList<>();
+        num2index = new HashMap<>();
+        rand = new Random();
     }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if (set.contains(val)) {
+        if (num2index.containsKey(val)) {
             return false;
         }
-        set.add(val);
-        list.add(val);
+        num2index.put(val, nums.size());
+        nums.add(val);
         return true;
     }
-
-    public boolean remove (int val) {
-        if (!set.contains(val)) {
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!num2index.containsKey(val)) {
             return false;
         }
-        set.remove(val);
-        list.remove(Integer.valueOf(val));
+        int index = num2index.get(val);
+        if (index != nums.size() - 1) {
+            int last = nums.get(nums.size() - 1);
+            nums.set(index, last);
+            num2index.put(last, index);
+        }
+        num2index.remove(val);
+        nums.remove(nums.size() - 1);
         return true;
     }
-
+    
+    /** Get a random element from the set. */
     public int getRandom() {
-        return list.get(random.nextInt(set.size()));
+        return nums.get(rand.nextInt(nums.size()));
     }
 }
 
